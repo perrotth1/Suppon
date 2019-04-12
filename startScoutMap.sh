32997a78ff2790bdebf0873afd2613c7314df357
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2019-04-11 22:48:08 -0400

    remove excess files

--- startScoutMap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[*] Starting Scout Map Server"
-
-IP=$(ifconfig | grep inet | grep -v 127.0.0.1 | grep -v inet6 | grep -m 1 inet | awk -F' ' '{print $2}')
-
-FLASK_APP=ifaceScout.py flask run --host=$IP
-
-
